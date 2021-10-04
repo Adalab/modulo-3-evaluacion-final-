e00e99080c0608422ee2243d8a06cec103961a37
Sararid <sarawen88@gmail.com>
2021-10-04 10:39:27 +0200

    add correction footer css

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 import '../styles/layout/NotFound.scss';
+import imgNotFound from "../images/notFound2.PNG";
+
 
 const PageNotFound = () => {
     return (
         <>
             <section className='wrapperNotFound'>
-                <p className='wrapperNotFound__errorText'>PAGE NOT FOUND</p>
 
-                <Link to="/" className='wrapperNotFound__returnText'>
+                <img
+                    className="wrapperNotFound__img"
+                    src={imgNotFound}
+                    alt="notFound"
+                />                <Link to="/" className='wrapperNotFound__returnText'>
                     <i class="fas fa-hand-point-left"></i>  Return to homepage
                 </Link></section>
         </>
